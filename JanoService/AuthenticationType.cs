cd89beab94376c66059746be6eecf0671251d381
sebastianoscarlopez <sebastianoscarlopez@gmail.com>
2018-04-20 14:48:16 -0300

    First Commit

---
+++ JanoService/AuthenticationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanoService
+{
+    [Serializable]
+    public enum AuthenticationType
+    {
+        RunAsPrompt, // when service is installed, the installer will prompt for a username and password
+        RunAsNetworkService, // runs as the NETWORK_SERVICE built-in account
+        RunAsLocalSystem, // run as the local system account
+        RunAsLocalService, // run as the local service account
+        RunAs // predefined user, It will used UserName and UserPassword key
+    }
+}
---
+++ JanoService/Service/WinService.cs
@@ -0,0 +1,78 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using Topshelf;
+
+namespace JanoService.Service
+{
+    class WinService
+    {
+
+        public ILog Log { get; private set; }
+        readonly Timer _timer;
+        public WinService(ILog logger)
+        {
+            /*
+            // IocModule.cs needs to be updated in case new paramteres are added to this constructor
+
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            Log = logger;
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+            */
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+
+            Log.Info($"{nameof(Service.WinService)} Start command received.");
+          //  _timer.Start();
+            return true;
+
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+
+            Log.Trace($"{nameof(Service.WinService)} Stop command received.");
+            //_timer.Stop();
+            return true;
+
+        }
+
+        public bool Pause(HostControl hostControl)
+        {
+
+            Log.Trace($"{nameof(Service.WinService)} Pause command received.");
+
+            //_timer.Stop();
+            return true;
+
+        }
+
+        public bool Continue(HostControl hostControl)
+        {
+
+            Log.Trace($"{nameof(Service.WinService)} Continue command received.");
+            //_timer.Start();
+            return true;
+
+        }
+
+        public bool Shutdown(HostControl hostControl)
+        {
+
+            Log.Trace($"{nameof(Service.WinService)} Shutdown command received.");
+            //_timer.Stop();
+            return true;
+
+        }
+
+    }
+}
